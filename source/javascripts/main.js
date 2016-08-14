521e35a35bc8ae6fd160e21a0a3cf2380fb6540b
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2016-08-14 14:46:14 -0700

    Make app interactive.

---
+++ source/javascripts/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  function loadingWait () {
+    return Math.random() * 3000;
+  };
+
+  var ui = {
+    $prompt: $(".js-prompt"),
+    $findOutButton: $(".js-find-out-button"),
+    $loadingIndicator: $(".js-loading-indicator"),
+    $result: $(".js-result"),
+    $tryAgain: $(".js-try-again"),
+  };
+
+  ui.$findOutButton.click(function() {
+    ui.$prompt.addClass("u-displayNone");
+    ui.$loadingIndicator.removeClass("u-displayNone");
+    setTimeout(function() {
+      ui.$loadingIndicator.addClass("u-displayNone");
+      ui.$result.removeClass("u-displayNone");
+    }, loadingWait());
+  });
+
+  ui.$tryAgain.click(function() {
+    location.reload();
+  });
+});
